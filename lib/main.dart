f72da692c4d661fe3f79881151703550ea13b2d4
irizzka <irizzavfx@gmail.com>
2020-01-17 23:17:02 +0200

    Quizzler app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,24 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> scoreKeeper = [];
+  QuizBrain quizBrain = QuizBrain();
+
+  void checkAnswer(bool userAnswer){
+    setState(() {
+      bool correctAnswer = quizBrain.getCorrectAnswer();
+      if(quizBrain.nextQuestion()){
+
+      if (correctAnswer == userAnswer) {
+        scoreKeeper.add(
+          Icon(Icons.done,color: Colors.green,),
+        );
+      }else{
+        scoreKeeper.add(Icon(Icons.clear, color: Colors.red,));
+      }
+    }});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +56,8 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+               // questions[questionNumber],
+                quizBrain.getQuestionText(),
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +81,7 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+               checkAnswer(true);
               },
             ),
           ),
@@ -79,19 +99,16 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+               checkAnswer(false);
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        ),
       ],
     );
   }
 }
 
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
